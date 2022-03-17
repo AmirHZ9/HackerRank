6983f685aff862d977d31a51151c0e9291452e49
unknown <amirzanganeh1999@gmail.com>
2022-03-17 09:45:54 +0330

    JumpingOnTheClouds

--- Warm-up Challenges/JumpingOnTheClouds.js
+++ Warm-up Challenges/JumpingOnTheClouds.js
@@ -1,12 +1,11 @@
-let jumpCount = [];
-
-const ar = [0, 0, 0, 1, 0, 0, 1, 0];
-
-for (let i = 0; i < ar.length; i += 2) {
-  if (ar[i] == 1) {
-    i -= 1;
+for (let i = 0; i <= ar.length; i++) {
+  if (ar[i + 2] == 0) {
+    jumpCount++;
+    i++;
+  } else if (ar[i + 2] == 1) {
+    jumpCount++;
+  } else if (ar[i + 1] == 0) {
+    jumpCount++;
   }
-  jumpCount.push(ar[i]);
 }
-
 console.log(jumpCount);
