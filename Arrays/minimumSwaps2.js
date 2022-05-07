9c8d54a72d1184b0363b36c31a53225ad25f2c33
unknown <amirzanganeh1999@gmail.com>
2022-05-07 17:13:37 +0430

    minimumSwap2

---
+++ Arrays/minimumSwaps2.js
@@ -0,0 +1,8 @@
+let minSwaps = 0;
+arr.map((item, index) => {
+  if (item !== index + 1) {
+    const rightIndex = arr.indexOf(index + 1, index);
+    [arr[index], arr[rightIndex]] = [arr[rightIndex], arr[index]];
+    ++minSwaps;
+  }
+});
