23d5efc336986f23e97c6bcef5c1763b17c41d57
unknown <amirzanganeh1999@gmail.com>
2023-04-25 20:47:45 +0330

    hackerRank-Dictionaries and Hashmaps-Hash Tables: Ransom Note

--- Dictionarys/Ransom note.js
+++ Dictionarys/Ransom note.js
@@ -1,3 +1,5 @@
+// challengs link :
+// https://www.hackerrank.com/challenges/ctci-ransom-note/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
 const magazine = "give me one grand today night";
 const note = "give one grand today";
 function makeFreqMap(str) {
