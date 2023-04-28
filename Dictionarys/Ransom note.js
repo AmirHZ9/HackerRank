32dbaa0cf291eb257575440129e23827cfd8b94f
unknown <amirzanganeh1999@gmail.com>
2023-04-28 19:42:27 +0330

    HackerRank-dictionaries-Ransome Note-second solution

--- Dictionarys/Ransom note.js
+++ Dictionarys/Ransom note.js
@@ -2,29 +2,49 @@
 // https://www.hackerrank.com/challenges/ctci-ransom-note/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
 const magazine = "two times three is not four";
 const note = "two times two is four";
-function makeFreqMap(str) {
-    const map = {};
-    for (let char of str) {
-        map[char] = map[char] + 1 || 1;
+const splitedMagazine = magazine.split(' ')
+const splitedNote = note.split(' ')
+
+
+// first solution
+splitedMagazine.map(item =>{
+    const index = splitedNote.indexOf(item)
+    if(index !== -1){
+        splitedNote.splice(index,1)
     }
-    return map;
+})
+if(note.length>0){
+    console.log("No")
+}else{
+    console.log("Yes")
+    
 }
-function checkMagazine(magazine, note) {
 
-    const magMap = makeFreqMap(magazine);
 
-    for (let word of note) {
-        if (!magMap[word]) {
-            console.log('No');
-            return;
-        }
 
-        magMap[word]--;
-    }
+// second solution
+// function makeFreqMap(str) {
+//     const map = {};
+//     for (let char of str) {
+//         map[char] = map[char] + 1 || 1;
+//     }
+//     return map;
+// }
+// function checkMagazine(magazine, note) {
 
-    console.log('Yes');
-cd
-}
-console.log(checkMagazine(magazine,note))
+//     const magMap = makeFreqMap(magazine);
 
+//     for (let word of note) {
+//         if (!magMap[word]) {
+//             console.log('No');
+//             return;
+//         }
+
+//         magMap[word]--;
+//     }
+
+//     console.log('Yes');
+// 
+// }
+// console.log(checkMagazine(magazine,note))
 
