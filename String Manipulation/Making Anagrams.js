334279c8c561eef25b5673f5fb18586604c916cf
unknown <amirzanganeh1999@gmail.com>
2023-04-28 19:30:41 +0330

    HackerRanck-String Manipulation-making Angrams

---
+++ String Manipulation/Making Anagrams.js
@@ -0,0 +1,18 @@
+const a = "cdec";
+const b = "dcf";
+let count = 0
+function makeAnagram(a, b) {
+    // Write your code here
+    const newA = a.split('')
+    const newB = b.split('')
+    let count =0
+     newA.map(item => {
+         const index = newB.indexOf(item)
+         if(index !== -1){
+             newB.splice(index,1)
+             count++
+         }
+     })
+     return newA.length-count+newB.length
+
+}
