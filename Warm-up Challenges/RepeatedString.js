7559aef84bf9b50f4673e7b76706c5a5c437129e
unknown <amirzanganeh1999@gmail.com>
2023-05-06 10:24:15 +0330

    HackerRank-warm-up challenges-Repeated String

--- Warm-up Challenges/RepeatedString.js
+++ Warm-up Challenges/RepeatedString.js
@@ -1,17 +1,25 @@
-function repeatString(string, number) {
-    let repeatedString = "";
-    while (number > 0) {
-      repeatedString += string;
-      number--;
-    }
-    return repeatedString;
-  }
+// challengs link:
+//https://www.hackerrank.com/challenges/repeated-string/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+const s ='aba'
+const n = 10
+let countA = 0
+let division = n/s.length
+let mod = n % s.length
+let add = 0;
+let result;
+for (let i=0; i<s.length; i++) {
+  if (s[i] === "a") {countA++}
+}
 
-  const s = "aba";
-  n = 10;
-  char = repeatString(s, n);
-  charCount = 0;
-  for (i = 0; i < n; i++) {
-    if (char[i] == "a") charCount++;
-  }
-  console.log(charCount);
+if (mod === 0) { 
+  result = countA * Math.trunc(division)
+  
+} else {
+  
+  for (let j=0; j<mod; j++) {
+      if (s[j] === "a") {add++}
+}
+result = countA * Math.trunc(division) + add
+
+}
+console.log(result)
