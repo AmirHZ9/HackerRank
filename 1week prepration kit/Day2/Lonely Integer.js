e5abd7d82a689e77177469ca00cb392804a0786f
unknown <amirzanganeh1999@gmail.com>
2023-05-08 22:59:49 +0330

    1 week challeng's-Day2-Digonal Diffrence

--- 1week prepration kit/Day2/Lonely Integer.js
+++ 1week prepration kit/Day2/Lonely Integer.js
@@ -1,4 +1,4 @@
-// challenge's link:
+// challeng's link:
 //https://www.hackerrank.com/challenges/one-week-preparation-kit-lonely-integer/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-two
 
 const a = [1, 2, 3, 4, 2, 3, 1];
